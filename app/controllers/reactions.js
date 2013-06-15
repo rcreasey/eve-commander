fac096ecdc625d72254df2feb4569a10e071ace7
Ryan C. Creasey <rcreasey@blizzard.com>
2013-06-15 11:54:44 -0700

    Fixing a bug; removing table-striped.

--- app/controllers/reactions.js
+++ app/controllers/reactions.js
@@ -1,5 +1,4 @@
 // GET /reactions/simple
 exports.simple = function (req, res) {
-  var reactions = require('../public/data/reactions-simple')
-  res.render('reactions/simple', {title: 'simple reactions', reactions: reactions})
+  res.render('reactions/simple', {title: 'simple reactions'})
 }
