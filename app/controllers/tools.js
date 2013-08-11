b37b32f3f68665aec0519f9c4b951246dbf9a57e
Ryan C. Creasey <rcreasey@blizzard.com>
2013-08-11 11:42:59 -0700

    Fixing up the inventory appraisal.

--- app/controllers/tools.js
+++ app/controllers/tools.js
@@ -25,51 +25,76 @@ exports.appraise = function (req, res) {
   }
 
   function findRefines(item, to_refine, data) {
-    refines = []
+    refines = new Object();
     for (var j = 0; j < to_refine.by_volume.length; j++) {
       if ( to_refine.by_volume[j] ) {
-        product = types[ data.component_index[j].toLowerCase() ]
-        refines.push({"id": product.typeID, 
+        var product = types[ data.component_index[j].toLowerCase() ];
+        var quantity = parseFloat( (to_refine.by_volume[j] * item.volume).toFixed(2) );
+        refines[product.name] = {"id": product.typeID, 
                     "name": product.name, 
-                    "quantity": parseFloat( (to_refine.by_volume[j] * item.volume).toFixed(2) ), 
-                    "volume": parseFloat( (product.volume * item.volume).toFixed(2) ),
+                    "quantity": quantity, 
+                    "volume": product.volume * quantity,
                     "refine": true,
-                    "parent": item.name})
+                    "parent": [item.name]};
       }
     }
 
-    return refines
+    return refines;
   }
 
-  var parsed_items = []
-  if (req.body.inventory) {
-    parsed_items = parser.parse( req.body.inventory.replace(/,/g,'') );
-  }
-
-  var inventory = []
-  parsed_items.map(function(item) {
-    i = types[item.name.toLowerCase()]
-
-    ice_refine = findByName( ice_data.ices, i.name )
-    ore_refine = findByName( ore_data.ores, i.name )
-
-    if (ice_refine) {
-      ice_refines = findRefines(item, ice_refine, ice_data)
-      for (var i in ice_refines ) {
-        inventory.push( ice_refines[i] )
+  function upsert(collection, item) {
+    if (item.name in collection) {
+      collection[ item.name ].quantity += item.quantity;
+      if (item.parent) {
+        collection[ item.name ].parent = collection[ item.name ].parent.concat(item.parent);
       }
-
-    } else if (ore_refine) {
-      ore_refines = findRefines(item, ore_refine, ore_data)
-      for (var i in ore_refines) {
-        inventory.push( ore_refines[i] )
-      }
-
     } else {
-      inventory.push({"id": i.typeID, "name": i.name, "quantity": item.quantity, "volume": item.volume})
+      collection[ item.name ] = item;
     }
+  }
+  
+  var parsed_items = new Array();
+  var unparsed  = new Array()
+  if (req.body.inventory) {
+    var lines = req.body.inventory.split("\r\n");
+    for (line in lines) {
+      try {
+        parsed_items.push(parser.parse( lines[line] )[0]);
+      } catch(e) {
+        unparsed.push({"error": e, "line": lines[line] });
+      }
+    }
+  }
 
-  })
+  var inventory = new Object();
+  parsed_items.map(function(item) {
+    if (! item.unparsed ) {
+      try {
+        var i = types[item.name.toLowerCase()];
 
-  res.render('tools/appraise', {title: 'Inventory Appraisal', inventory: inventory})
+        ice_refine = findByName( ice_data.ices, i.name );
+        ore_refine = findByName( ore_data.ores, i.name );
+
+        if (ice_refine) {
+          ice_refines = findRefines(item, ice_refine, ice_data);
+          for (var i in ice_refines ) {
+            upsert(inventory, ice_refines[i]);
+          }
+
+        } else if (ore_refine) {
+          ore_refines = findRefines(item, ore_refine, ore_data);
+          for (var i in ore_refines) {
+            upsert(inventory, ore_refines[i]);
+          }
+
+        } else {
+          upsert(inventory, {"id": i.typeID, "name": i.name, "quantity": item.quantity, "volume": i.volume * item.quantity});
+        }
+      } catch (e) {
+        unparsed.push({"error": e, "item": item});
+      }
+    }
+  });
+
+  res.render('tools/appraise', {title: 'Inventory Appraisal', inventory: inventory, unparsed: unparsed, parsed_items: parsed_items});
 }
